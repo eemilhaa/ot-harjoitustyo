999586c70bcae0172b76dc3c0868bb0ca1d02632
eemilhaa <eemil.haapanen@gmail.com>
2022-04-05 12:52:49 +0300

    test both moving directions

--- src/tests/player_test.py
+++ src/tests/player_test.py
@@ -6,7 +6,7 @@ from sprites.background import BackGround1
 
 MAP_1 = [
     [0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1],
 ]
@@ -15,14 +15,19 @@ MAP_1 = [
 class TestPlayer(unittest.TestCase):
     def setUp(self):
         self.level = Level(
-            player=Player(0, 0),
+            player=Player(10, 0),
             game_map=MAP_1,
             background=BackGround1()
         )
 
-    def test_can_move_right(self):
-        collide_rects = self.level.ground
+    def test_can_move_left(self):
         pos1 = self.level.player.rect.x
-        self.level.player.move_right(collide_rects)
+        self.level.player.move_left(self.level.ground)
+        pos2 = self.level.player.rect.x
+        self.assertTrue(pos1 > pos2)
+
+    def test_can_move_right(self):
+        pos1 = self.level.player.rect.x
+        self.level.player.move_right(self.level.ground)
         pos2 = self.level.player.rect.x
         self.assertTrue(pos1 < pos2)
