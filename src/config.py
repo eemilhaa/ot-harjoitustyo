046fce46c3976dddd4420152f10dc3f9b16f1561
eemilhaa <eemil.haapanen@gmail.com>
2022-04-26 16:21:02 +0300

    add database draft

--- src/config.py
+++ src/config.py
@@ -1,1 +1,9 @@
+import os
+import sqlite3
+
+dirname = os.path.dirname(__file__)
+
 display_height = 900
+
+
+database_file_path = os.path.join(dirname, '..', "data", "database.db")
--- src/database.py
+++ src/database.py
@@ -1,12 +1,60 @@
 import sqlite3
+from config import database_file_path
+
 
 class DataBase:
     def __init__(self):
-        self.database = sqlite3.connect("")
+        self.connection = self.create_database()
+        try:
+            self.create_table()
+        except sqlite3.OperationalError:
+            pass
 
-    def store(self, arg):
-        pass
+    def store_result(self, result: int):
+        self.connection.execute(
+            """
+            INSERT INTO GameRuns (level) VALUES (?)
+            """,
+            [result]
+        )
 
-    def query(self, arg):
-        pass
+    def query_highscore(self):
+        return self.connection.execute(
+            """
+            SELECT MAX(level)
+            FROM GameRuns
+            """
+        ).fetchone()[0]
 
+    def query_number_of_runs(self):
+        return self.connection.execute(
+            """
+            SELECT COUNT(DISTINCT(id))
+            FROM GameRuns
+            """
+        ).fetchone()[0]
+
+    def create_database(self):
+        connection = sqlite3.connect(database_file_path)
+        connection.isolation_level = None
+        return connection
+
+    def create_table(self):
+        
+        self.connection.execute(
+            """
+            CREATE TABLE GameRuns (
+                id INTEGER PRIMARY KEY,
+                level INTEGER
+            );
+            """
+        )
+
+
+if __name__ == "__main__":
+    db = DataBase()
+    db.store_result(4)
+    db.store_result(3)
+    db.store_result(1)
+    print(db.query_highscore())
+    print(db.query_number_of_runs())
