7efd381d9cfff1ca58223787f33d3939a65a65f2
eemilhaa <eemil.haapanen@gmail.com>
2022-05-15 20:12:56 +0300

    take invalid input into account

--- src/config.py
+++ src/config.py
@@ -12,6 +12,12 @@ def get_display_size(display_height):
         A tuple with the display dimension. The aspect ratio is always 4:3.
     """
 
+    try:
+        display_height = int(display_height)
+    except ValueError:
+        display_height = 900
+        print("invalid display height value in .env, using defaults")
+
     return (
         display_height / 0.75,
         display_height
@@ -23,11 +29,11 @@ dirname = os.path.dirname(__file__)
 try:
     load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
 except FileNotFoundError:
-    print("no .env file found, using defaults")
+    pass
 
 
 DISPLAY_SIZE = get_display_size(
-    display_height=int(os.getenv("DISPLAY_HEIGHT")) or 900
+    display_height=os.getenv("DISPLAY_HEIGHT") or 900
 )
 
 DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.db"
